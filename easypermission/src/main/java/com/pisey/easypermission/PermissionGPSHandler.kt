dab38eade473adca9a32023536ac8b18c02ed7b9
Pisey Nguon <n.sey168@gmail.com>
2024-01-19 15:09:20 +0700

    Reduce Resource

--- easypermission/src/main/java/com/pisey/easypermission/PermissionGPSHandler.kt
+++ easypermission/src/main/java/com/pisey/easypermission/PermissionGPSHandler.kt
@@ -1,20 +1,35 @@
 package com.pisey.easypermission
 
 import android.content.Context
-import androidx.activity.result.IntentSenderRequest
+import android.content.Intent
+import android.location.LocationManager
+import android.provider.Settings
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 class PermissionGPSHandler(private val fragment: Fragment, private val forceGranted: Boolean) {
 
 
     private var permissionGPSExecutor: (() -> Unit)? = null
-    private var intentSenderRequest:IntentSenderRequest? = null
 
     private val context: Context by lazy { fragment.requireContext() }
 
-    private val resolutionForResult = fragment.registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) {
-        if (it.resultCode == AppCompatActivity.RESULT_OK) permissionGPSExecutor?.invoke() else showDialogForceGPSPermissionDenied()
+    private val gpsRequiredResult = fragment.registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if (isLocationEnabled()) {
+            // Do something with the location updates
+            permissionGPSExecutor?.invoke()
+        } else {
+            // The user did not allow the location services, handle it accordingly
+            showDialogForceGPSPermissionDenied()
+        }
+    }
+
+    private fun isLocationEnabled():Boolean{
+        // Get the LocationManager instance
+        val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        // Check if the GPS provider or the network provider are enabled
+        val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        val isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        return isGpsEnabled || isNetworkEnabled
     }
 
     private fun showDialogForceGPSPermissionDenied() {
@@ -24,18 +39,16 @@ class PermissionGPSHandler(private val fragment: Fragment, private val forceGran
             message = "Please turn on GPS to use this feature.",
             positiveButtonTitle = "Turn On",
             negativeButtonTitle = if (!forceGranted) "Don't Allow" else null,
-            onPositionClicked = { intentSenderRequest?.let {resolutionForResult.launch(it)  }  }
+            onPositionClicked = {  gpsRequiredResult.launch(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))}
         )
     }
 
     fun runGPSPermission(execute: () -> Unit) {
         permissionGPSExecutor = execute
-        LocationUtils.enableLoc(fragment.requireActivity(),
-            allowRequest = { permissionGPSExecutor?.invoke() },
-            launchRequest = {
-                intentSenderRequest = it
-                resolutionForResult.launch(it)
-            }
-        )
+        if (isLocationEnabled()){
+            execute.invoke()
+        }else{
+            gpsRequiredResult.launch(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+        }
     }
 }
